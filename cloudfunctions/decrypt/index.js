aefded3b880f26af84fb6ed33f1799443a23bbf6
xhute <xhute@xhutedeMacBook-Pro.local>
2018-09-15 18:13:02 +0800

    1.init project from wxcloud quickstart ;2.cloud get openGid

---
+++ cloudfunctions/decrypt/index.js
@@ -0,0 +1,21 @@
+const WXBizDataCrypt = require('./WXBizDataCrypt')
+const requestSync = require('./requestSync')
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const code = event.data.js_code
+  const appid = event.userInfo.appId
+  const encryptedData = event.data.encryptedData
+  const iv = event.data.iv
+  const secret = 'cb7bb0ee454df970774904fe34f42ada'
+
+  const url = {
+    url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code'
+  }
+  const req = await requestSync(url);
+  const session = JSON.parse(req);
+  const sessionKey = session.session_key;
+  const pc = new WXBizDataCrypt(appid, sessionKey)
+  const data = pc.decryptData(encryptedData, iv)
+
+  return data;
+}
---
+++ miniprogram/app.js
@@ -0,0 +1,48 @@
+//app.js
+App({
+  onLaunch: function (opt) {
+    console.log(opt)
+    if (opt.shareTicket) {
+      wx.getShareInfo({
+        shareTicket: opt.shareTicket,
+        success: shareInfo => {
+          wx.login({
+            success: loginInfo => {
+              wx.cloud.callFunction({
+                name: 'decrypt',
+                data: {
+                  data: {
+                    js_code: loginInfo.code,
+                    encryptedData: shareInfo.encryptedData,
+                    iv: shareInfo.iv
+                  }
+                },
+                success: cloudFunInfo => {
+                  console.log(cloudFunInfo.result)
+                },
+                fail: err => {
+                  console.log('callFunction err:', err)
+                }
+              })
+            }
+          })
+        },
+        fail: err => {
+          console.log("getShareInfo error:" + err)
+        }
+      })
+    }
+
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+      })
+    }
+
+    this.globalData = {}
+
+    this.globalData.scene = opt.scene;
+  }
+})
